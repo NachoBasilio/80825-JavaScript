e209f6469c2347cd6d150b19344442409abcd248
NachoBasilio <ignacio.n.basilio.b@gmail.com>
2025-06-24 21:06:45 -0300

    primer commit

---
+++ Semana-1/script/main.js
@@ -0,0 +1,32 @@
+let edad1 = 20
+let edad2 = '3'
+const edad3 = 49
+
+console.log(edad1)
+console.log(edad2)
+console.log(true)
+console.log('true')
+
+console.log(typeof true)
+
+console.log(0.1 + 0.2)
+
+// edad2 = Number(edad2) + 2
+// edad2 = parseInt(edad2) + 2
+// edad2 = parseFloat(edad2) + 2
+
+console.log(isNaN(Number(edad2)))
+
+alert('Hola soy una alerta!')
+
+let comoSePortoElGato = confirm('¿El gato se porto mal?')
+
+console.log(comoSePortoElGato)
+
+let nombre = prompt('Como te llamas')
+
+console.log(nombre)
+
+let direccion
+
+console.log(typeof direccion)
